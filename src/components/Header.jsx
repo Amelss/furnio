36542811b6729293e2f92bcc3c8785c7d7720d74
Amelss <ameleyka@gmail.com>
2023-09-04 17:46:38 +0100

    Initial commit on new branch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,8 @@
 export default function Header() {
   return (
       <div>
-          <h1 className="text-red-500">Header</h1>
+      <h1 className="text-red-500">Header</h1>
+      <h2>Second</h2>
       </div>
   )
 }
