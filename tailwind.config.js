9c95383cb76a499952918b58589305739c7c2bc9
Amelss <ameleyka@gmail.com>
2023-09-05 14:56:51 +0100

    Adding custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat"],
+        poppins: ["Poppins"],
+      },
+    },
   },
   plugins: [],
 };
