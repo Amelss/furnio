dc83ee2d6cd1af8460cf44003ce84ed87237bb15
Amelss <ameleyka@gmail.com>
2023-09-05 16:54:15 +0100

    Added logo and site name & corrected site name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,22 @@
+import { Link } from "react-router-dom"
+
 
 export default function Header() {
   return (
       <div>
-      <h1 className="text-red-500">Header</h1>
+      <div className="">
+        <div>
+          <div className="flex items-center p-3 md:p-8">
+            <img src="./src/assets/logo.png" alt="logo" className="w-10" />
+            <h2 className="font-montserrat font-bold ml-2 text-3xl">Furniro</h2>
+          </div>
+          
+        </div>
+        <div>
+          <Link to={"/"}> </Link>
+        </div>
+    
+      </div>
     
       </div>
   )
