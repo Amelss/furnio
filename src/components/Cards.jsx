41a0b7442b408ee318e5b58e66d112defe720143
Amelss <ameleyka@gmail.com>
2023-09-06 21:30:15 +0100

    Adding title styles and created button

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -25,29 +25,37 @@ export default function Cards() {
 
   return (
     <div className="">
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-5 p-3 content-center items-center font-poppins ">
+      <div className="grid grid-cols-2 md:grid-cols-4 2xl:grid-cols-4 2xl:gap-x-0 gap-5 place-items-center p-3 font-poppins ">
         {productData.map((products) => (
           <div key={id}>
-            <div className="md:w-[300px]">
+            <div className="md:w-[300px] 2xl:w-[400px]">
               <div>
                 <img
                   src={products.img}
                   alt="product image"
-                  className="w-[300px] h-[200px] md:w-[300px] md:h-[400px]"
+                  className="w-[300px] h-[200px] md:w-[300px] md:h-[400px] 2xl:w-[500px] 2xl:h-[400px] "
                 />
                 <div>{products.new}</div>
               </div>
 
               <div className="bg-cardGrey p-3 w-full h-28">
-                <h1 className="text-xl text-headingGrey pb-1">{products.productName}</h1>
-                <p className="text-sm text-cardTextGrey pb-1">{products.description}</p>
+                <h1 className="text-xl text-headingGrey pb-1">
+                  {products.productName}
+                </h1>
+                <p className="text-sm text-cardTextGrey pb-1">
+                  {products.description}
+                </p>
 
                 {products.beforePrice > 1 ? (
                   <p className="text-base text-headingGrey">
-                  £{products.price} <span className="text-lineThroughGrey line-through">£{products.beforePrice}</span>
-                </p>
-                ) : (<p>£{products.price}</p> )}
-                
+                    £{products.price}{" "}
+                    <span className="text-lineThroughGrey line-through">
+                      £{products.beforePrice}
+                    </span>
+                  </p>
+                ) : (
+                  <p>£{products.price}</p>
+                )}
               </div>
             </div>
           </div>
--- src/sections/OurProducts.jsx
+++ src/sections/OurProducts.jsx
@@ -3,12 +3,14 @@ import Cards from '../components/Cards'
 
 export default function OurProducts() {
   return (
-    <div className='font-poppins'>
-      <h1>OurProducts</h1>
+    <div className="font-poppins ">
+      <h1 className="text-headingGrey text-[40px] font-bold text-center pb-8">
+        Our Products
+      </h1>
       <Cards />
-      <button>CLICK ME</button>
+      <div className="text-center py-10">
+        <button className="text-gold border border-gold px-20 py-3 text-base">Show More</button>
+      </div>
     </div>
-   
-      
-  )
+  );
 }
