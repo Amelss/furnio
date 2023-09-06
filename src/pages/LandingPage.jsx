c85801124c6bf293249e8b3a12140436379dd42c
Amelss <ameleyka@gmail.com>
2023-09-06 16:00:48 +0100

    Creating products component

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,12 +1,13 @@
 import BrowseRange from "../components/BrowseRange";
 import Hero from "../components/Hero";
-
+import OurProducts from "../components/OurProducts";
 
 export default function LandingPage() {
     return (
         <div>
             <Hero />
             <BrowseRange />
+            <OurProducts />
         </div>
     )
 }
