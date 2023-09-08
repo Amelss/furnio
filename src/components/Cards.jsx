93d5fb11cbe5b5425dc8d24eb5343c988f205942
Amelss <ameleyka@gmail.com>
2023-09-08 21:09:49 +0100

    Adding product badges

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -30,16 +30,39 @@ export default function Cards() {
           <div key={id}>
             <div className="md:w-[300px] 2xl:w-[400px]">
               <div>
-                <img
-                  src={products.img}
-                  alt="product image"
-                  className="w-[300px] h-[200px] md:w-[300px] md:h-[400px] 2xl:w-[500px] 2xl:h-[400px] "
-                />
-                <div>{products.new}</div>
+                <div className="relative">
+                  <img
+                    src={products.img}
+                    alt="product image"
+                    className="w-[300px] h-[200px] md:w-[300px] md:h-[400px] 2xl:w-[500px] 2xl:h-[400px] "
+                  />
+               
+                  {products.discount < 0 ? (
+                    <div className="top-5 absolute right-5 bg-badgeRed rounded-[200%] h-12 w-12 text-center">
+                    <p className="  text-white  mt-3">
+                      {products.discount}%
+                    </p>
+                    </div>) : (
+                        <p></p>
+                  )} 
+                  
+
+                  {products.new === true ? (
+                    <div className="top-5 absolute right-5 bg-badgeGreen rounded-[200%] h-12 w-12 text-center">
+                    <p className="  text-white  mt-3">
+                     {products.new} New
+                    </p>
+                  </div>
+                  ) : (
+                    <p></p>
+                  )}
+                  
+                </div>
+
               </div>
 
               <div className="bg-cardGrey p-3 w-full h-28">
-                <h1 className="text-xl text-headingGrey pb-1">
+                <h1 className="text-xl text-headingGrey pb-1 font-bold">
                   {products.productName}
                 </h1>
                 <p className="text-sm text-cardTextGrey pb-1">
